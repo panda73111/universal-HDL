0aa3590231a40b8e8ebfd214f197c3b014ca55e5
Sebastian Hüther <sebastian.huether@gmx.de>
2014-08-05 17:39:19 +0200

    Changed signal names, read/write first mode switch

--- RAM/DUAL_PORT_RAM.vhd
+++ RAM/DUAL_PORT_RAM.vhd
@@ -1,47 +1,50 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-library UNISIM;
-use UNISIM.VComponents.all;
-
--- Write First Mode
+use work.help_funcs.all;
 
 entity DUAL_PORT_RAM is
     generic (
         -- default: 1 Kilobyte in bytes
-        ADDR_WIDTH  : integer := 10;
-        DATA_WIDTH  : integer := 8
+        WIDTH       : integer := 8;
+        DEPTH       : integer := 1024;
+        READ_FIRST  : boolean := false
     );
     port (
         CLK : in std_ulogic;
 
-        RD_ADDR : in std_ulogic_vector(ADDR_WIDTH - 1 downto 0);
+        RD_ADDR : in std_ulogic_vector(log2(DEPTH)-1 downto 0);
         WR_EN   : in std_ulogic;
-        WR_ADDR : in std_ulogic_vector(ADDR_WIDTH - 1 downto 0);
-        DATA_IN : in std_ulogic_vector(DATA_WIDTH - 1 downto 0);
+        WR_ADDR : in std_ulogic_vector(log2(DEPTH)-1 downto 0);
+        DIN     : in std_ulogic_vector(WIDTH-1 downto 0);
 
-        DATA_OUT    : out std_ulogic_vector(DATA_WIDTH - 1 downto 0) := (others => '0')
+        DOUT    : out std_ulogic_vector(WIDTH-1 downto 0) := (others => '0')
     );
 end DUAL_PORT_RAM;
 
 architecture Behavioral of DUAL_PORT_RAM is
-    type ram_type is array (0 to 2 ** ADDR_WIDTH - 1) of std_ulogic_vector(DATA_WIDTH - 1 downto 0);
-    signal ram  : ram_type := (others => (others => '0'));
+    
+    type ram_type is
+        array (0 to DEPTH-1) of
+        std_ulogic_vector(WIDTH-1 downto 0);
+    
+    signal ram  : ram_type;
+    
 begin
 
-    process(CLK)
+    ram_ctrl_proc : process(CLK)
     begin
         if rising_edge(CLK) then
+            
+            DOUT    <= ram(int(RD_ADDR));
+            
             if WR_EN = '1' then
-                ram(to_integer(unsigned(WR_ADDR))) <= DATA_IN;
+                ram(int(WR_ADDR))   <= DIN;
+                if not READ_FIRST and WR_ADDR=RD_ADDR then
+                    DOUT    <= DIN;
+                end if;
             end if;
-
-            DATA_OUT <= ram(to_integer(unsigned(RD_ADDR)));
-
-            if WR_EN = '1' and WR_ADDR = RD_ADDR then
-                DATA_OUT  <= DATA_IN;
-            end if;
+            
         end if;
     end process;
 
