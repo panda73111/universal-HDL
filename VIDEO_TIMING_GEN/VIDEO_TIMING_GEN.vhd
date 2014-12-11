5789f531ac50eb6f31681d756a763756a128ac8f
Sebastian Hüther <sebastian.huether@gmx.de>
2014-12-12 00:24:55 +0100

    First functioning version of VIDEO_TIMING_GEN (not fully tested)

--- VIDEO_TIMING_GEN/VIDEO_TIMING_GEN.vhd
+++ VIDEO_TIMING_GEN/VIDEO_TIMING_GEN.vhd
@@ -18,16 +18,20 @@ use work.video_profiles.all;
 
 entity VIDEO_TIMING_GEN is
     generic (
-        CLK_IN_PERIOD   : real;
-        PROFILE_BITS    : natural := log2(VIDEO_PROFILE_COUNT);
-        X_BITS          : natural := 11;
-        Y_BITS          : natural := 11
+        CLK_IN_PERIOD           : real;
+        CLK_IN_TO_CLK10_MULT    : natural := 1;
+        CLK_IN_TO_CLK10_DIV     : natural := 2;
+        PROFILE_BITS            : natural := log2(VIDEO_PROFILE_COUNT);
+        X_BITS                  : natural := 11;
+        Y_BITS                  : natural := 11
     );
     port (
-        CLK : in std_ulogic;
-        RST : in std_ulogic;
+        CLK_IN  : in std_ulogic;
+        RST     : in std_ulogic;
         
         PROFILE : in std_ulogic_vector(PROFILE_BITS-1 downto 0);
+        
+        CLK_OUT : out std_ulogic := '0';
         
         POS_VSYNC   : out std_ulogic := '0';
         POS_HSYNC   : out std_ulogic := '0';
@@ -44,49 +48,62 @@ architecture rtl of VIDEO_TIMING_GEN is
     type state_type is (
         HOR_SYNC,
         HOR_FRONT_PORCH,
+        LEFT_BORDER,
         PIXEL,
-        HOR_BACK_PORCH,
-        LINE_SWITCH,
-        FRAME_SWITCH
+        RIGHT_BORDER,
+        HOR_BACK_PORCH
     );
     
     type reg_type is record
-        state       : state_type;
-        x           : natural range 0 to 2**X_BITS-1;
-        y           : natural range 0 to 2**Y_BITS-1;
-        rgb_enable  : std_ulogic;
-        pos_vsync   : std_ulogic;
-        pos_hsync   : std_ulogic;
-        other_frame : boolean;
+        state           : state_type;
+        x               : natural range 0 to 2**X_BITS-1;
+        y               : natural range 0 to 2**Y_BITS-1;
+        ver_rgb_enable  : std_ulogic;
+        hor_rgb_enable  : std_ulogic;
+        pos_vsync       : std_ulogic;
+        pos_hsync       : std_ulogic;
+        other_frame     : boolean;
     end record;
     
     constant reg_type_def   : reg_type := (
-        state       => HOR_SYNC,
-        x           => 0,
-        y           => 0,
-        rgb_enable  => '0',
-        pos_vsync   => '0',
-        pos_hsync   => '0',
-        other_frame => false
+        state           => HOR_SYNC,
+        x               => 0,
+        y               => 0,
+        ver_rgb_enable  => '0',
+        hor_rgb_enable  => '0',
+        pos_vsync       => '0',
+        pos_hsync       => '0',
+        other_frame     => false
     );
     
     signal cur_reg, next_reg    : reg_type := reg_type_def;
     
+    signal pix_clk  : std_ulogic := '0';
+    
     signal vp               : video_profile_type;
     signal total_ver_lines  : natural range 0 to 2**(Y_BITS+1)-1;
     signal total_hor_pixels : natural range 0 to 2**(X_BITS+1)-1;
+    signal clk_locked       : std_ulogic := '0';
+    
+    signal cur_profile  : std_ulogic_vector(PROFILE_BITS-1 downto 0) := (others => '0');
+    signal profile_set  : boolean := false;
+    signal reprog_en    : std_ulogic := '0';
+    signal rst_stm      : std_ulogic := '1';
     
 begin
+    
+    CLK_OUT <= pix_clk;
     
     POS_VSYNC   <= cur_reg.pos_vsync;
     POS_HSYNC   <= cur_reg.pos_hsync;
     VSYNC       <= cur_reg.pos_vsync xor sel(vp.negative_vsync, '1', '0');
     HSYNC       <= cur_reg.pos_hsync xor sel(vp.negative_hsync, '1', '0');
+    RGB_ENABLE  <= cur_reg.ver_rgb_enable and cur_reg.hor_rgb_enable;
     
     vp  <= video_profiles(int(PROFILE));
     
     total_ver_lines     <= vp.v_sync_lines + vp.v_front_porch + vp.top_border + vp.height +
-                            vp.bottom_border + vp.v_back_porch;
+                            vp.bottom_border + vp.v_back_porch + sel(vp.interlaced, 1, 0);
     
     total_hor_pixels    <= vp.h_sync_cycles + vp.h_front_porch + vp.left_border + vp.width +
                             vp.right_border + vp.h_back_porch;
@@ -98,33 +115,72 @@ begin
             DIVISOR         => 2
         )
         port map (
-            CLK_IN  => CLK,
-            RST     => RST
+            CLK_IN  => CLK_IN,
+            RST     => RST,
+            
+            REPROG_MULT => stdulv(vp.clk10_mult*CLK_IN_TO_CLK10_MULT, 8),
+            REPROG_DIV  => stdulv(vp.clk10_div*CLK_IN_TO_CLK10_DIV, 8),
+            REPROG_EN   => reprog_en,
+            
+            CLK_OUT => pix_clk,
+            LOCKED  => clk_locked
         );
     
-    stm_proc : process(RST, cur_reg, vp)
+    dcm_reprog_proc : process(RST, CLK_IN)
+    begin
+        if RST='1' then
+            cur_profile <= (others => '0');
+            profile_set <= false;
+            reprog_en   <= '0';
+            rst_stm     <= '1';
+        elsif rising_edge(CLK_IN) then
+            rst_stm     <= '0';
+            reprog_en   <= '0';
+            if PROFILE/=cur_profile then
+                profile_set <= false;
+            end if;
+            if
+                not profile_set and
+                clk_locked='1'
+            then
+                reprog_en   <= '1';
+                cur_profile <= PROFILE;
+                profile_set <= true;
+                rst_stm     <= '1';
+            end if;
+        end if;
+    end process;
+    
+    stm_proc : process(rst_stm, cur_reg, vp, PROFILE)
         alias cr is cur_reg;
         alias x is cr.x;
         alias y is cr.y;
         variable r  : reg_type := reg_type_def;
     begin
-        r               := cr;
-        r.pos_vsync     := '0';
-        r.pos_hsync     := '0';
-        r.rgb_enable    := '0';
-        r.x             := cr.x+1;
+        r                   := cr;
+        r.pos_hsync         := '0';
+        r.hor_rgb_enable    := '0';
+        r.x                 := cr.x+1;
         
         if
             not vp.interlaced or
             not cr.other_frame or
-            x >= total_hor_pixels/2
+            x=total_hor_pixels/2
         then
-            if y < vp.v_sync_lines then
+            if y=0 then
                 -- vsync period
                 r.pos_vsync := '1';
-            else
+            end if;
+            if y=vp.v_sync_lines then
                 r.pos_vsync := '0';
             end if;
+        end if;
+        
+        if y=vp.v_sync_lines+vp.v_front_porch+vp.top_border then
+            r.ver_rgb_enable    := '1';
+        end if;
+        if y=vp.v_sync_lines+vp.v_front_porch+vp.top_border+vp.height then
+            r.ver_rgb_enable    := '0';
         end if;
         
         case cr.state is
@@ -136,53 +192,69 @@ begin
                 end if;
             
             when HOR_FRONT_PORCH =>
+                if x=vp.h_sync_cycles+vp.h_front_porch-1 then
+                    r.state := LEFT_BORDER;
+                    if vp.left_border=0 then
+                        r.state := PIXEL;
+                    end if;
+                end if;
+            
+            when LEFT_BORDER =>
                 if x=vp.h_sync_cycles+vp.h_front_porch+vp.left_border-1 then
                     r.state := PIXEL;
                 end if;
             
             when PIXEL =>
-                r.rgb_enable    := '1';
+                r.hor_rgb_enable    := '1';
                 if x=vp.h_sync_cycles+vp.h_front_porch+vp.left_border+vp.width-1 then
+                    r.state := RIGHT_BORDER;
+                    if vp.right_border=0 then
+                        r.state := HOR_BACK_PORCH;
+                    end if;
+                end if;
+            
+            when RIGHT_BORDER =>
+                if x=vp.h_sync_cycles+vp.h_front_porch+vp.left_border+vp.width+vp.right_border-1 then
                     r.state := HOR_BACK_PORCH;
                 end if;
             
             when HOR_BACK_PORCH =>
                 if x=total_hor_pixels-1 then
-                    r.state := LINE_SWITCH;
+                    -- line switch
+                    r.x := 0;
+                    r.y := y+1;
+                    if
+                        vp.interlaced and
+                        not cr.other_frame and
+                        y=vp.v_sync_lines+vp.v_front_porch-1
+                    then
+                        -- skip the additional interlacing
+                        -- blank line in even frames
+                        r.y := y+2;
+                    end if;
+                    if cr.y=total_ver_lines-1 then
+                        -- frame switch
+                        r.y             := 0;
+                        r.other_frame   := not cr.other_frame;
+                    end if;
+                    r.state := HOR_SYNC;
                 end if;
-            
-            when LINE_SWITCH =>
-                r.x := 0;
-                r.y := y+1;
-                if cr.y=total_ver_lines-1 then
-                    r.state := FRAME_SWITCH;
-                end if;
-            
-            when FRAME_SWITCH =>
-                r.x             := 0;
-                r.y             := 0;
-                r.other_frame   := not cr.other_frame;
-                r.state         := HOR_SYNC;
             
         end case;
         
-        if x=total_hor_pixels-2 then
-            r.state := LINE_SWITCH;
-        end if;
-        
-        if RST='1' then
+        if rst_stm='1' then
             r   := reg_type_def;
         end if;
         
         next_reg    <= r;
     end process;
     
-    stm_sync_proc : process(RST, CLK)
+    stm_sync_proc : process(rst_stm, pix_clk)
     begin
-        if RST='1' then
-            next_reg    <= reg_type_def;
-        elsif rising_edge(CLK) then
-            next_reg    <= cur_reg;
+        if rst_stm='1' then
+            cur_reg <= reg_type_def;
+        elsif rising_edge(pix_clk) then
+            cur_reg <= next_reg;
         end if;
     end process;
     
--- VIDEO_TIMING_GEN/VIDEO_TIMING_GEN_tb.vhd
+++ VIDEO_TIMING_GEN/VIDEO_TIMING_GEN_tb.vhd
@@ -19,40 +19,89 @@ use work.help_funcs.all;
 use work.video_profiles.all;
 
 ENTITY VIDEO_TIMING_GEN_tb IS
+    generic (
+        PROFILE_BITS    : natural := log2(VIDEO_PROFILE_COUNT);
+        X_BITS          : natural := 11;
+        Y_BITS          : natural := 11
+    );
 END VIDEO_TIMING_GEN_tb;
 
 ARCHITECTURE rtl OF VIDEO_TIMING_GEN_tb IS
 
     -- Inputs
-    signal CLK  : std_ulogic := '0';
-    signal RST  : std_ulogic := '0';
+    signal CLK_IN   : std_ulogic := '0';
+    signal RST      : std_ulogic := '0';
     
     signal PROFILE  : std_ulogic_vector(PROFILE_BITS-1 downto 0) := (others => '0');
     
     -- Outputs
-    signal POS_VSYNC    : std_ulogic := '0';
-    signal POS_HSYNC    : std_ulogic := '0';
-    signal VSYNC        : std_ulogic := '0';
-    signal HSYNC        : std_ulogic := '0';
-    signal RGB_ENABLE   : std_ulogic := '0';
-    signal X            : std_ulogic_vector(X_BITS-1 downto 0) := (others => '0');
-    signal Y            : std_ulogic_vector(Y_BITS-1 downto 0) := (others => '0');
+    signal CLK_OUT  : std_ulogic;
     
-    constant FRAME_COUNT    : natural := 10;
+    signal POS_VSYNC    : std_ulogic;
+    signal POS_HSYNC    : std_ulogic;
+    signal VSYNC        : std_ulogic;
+    signal HSYNC        : std_ulogic;
+    signal RGB_ENABLE   : std_ulogic;
+    signal X            : std_ulogic_vector(X_BITS-1 downto 0);
+    signal Y            : std_ulogic_vector(Y_BITS-1 downto 0);
+    
+    constant FRAME_COUNT    : natural := 4;
+    
+    constant CLK_IN_period      : time := 50 ns; -- 20 MHz
+    constant CLK_IN_period_real : real := real(CLK_IN_period / 1 ps) / real(1 ns / 1 ps);
     
 BEGIN
+    
+    VIDEO_TIMING_GEN_inst : entity work.VIDEO_TIMING_GEN
+        generic map (
+            CLK_IN_PERIOD           => clk_in_period_real,
+            CLK_IN_TO_CLK10_MULT    => 1,
+            CLK_IN_TO_CLK10_DIV     => 2,
+            PROFILE_BITS            => PROFILE_BITS,
+            X_BITS                  => X_BITS,
+            Y_BITS                  => Y_BITS
+        )
+        port map (
+            CLK_IN  => CLK_IN,
+            RST     => RST,
+            
+            PROFILE => PROFILE,
+            
+            CLK_OUT => CLK_OUT,
+            
+            POS_VSYNC   => POS_VSYNC,
+            POS_HSYNC   => POS_HSYNC,
+            VSYNC       => VSYNC,
+            HSYNC       => HSYNC,
+            RGB_ENABLE  => RGB_ENABLE,
+            X           => X,
+            Y           => Y
+        );
+    
+    CLK_IN  <= not CLK_IN after CLK_IN_period/2;
     
     -- Stimulus process
     stim_proc: process
     begin
         -- hold reset state for 100 ns.
         RST <= '1';
-        wait for 100 ns;
+        wait for 1 us;
         RST <= '0';
         wait for 100 ns;
-        wait until rising_edge(pix_clk);
+        wait until rising_edge(CLK_IN);
         
-        -- insert stimulus here 
+        -- insert stimulus here
+        
+        for profile_i in 0 to VIDEO_PROFILE_COUNT-1 loop
+            report "Setting profile " & natural'image(profile_i);
+            PROFILE <= stdulv(profile_i, PROFILE_BITS);
+            
+            for frame_i in 0 to FRAME_COUNT-1 loop
+                wait until POS_VSYNC='1';
+                wait until POS_VSYNC='0';
+            end loop;
+            wait for 100 ns;
+        end loop;
         
         wait for 100 ns;
         report "NONE. All tests completed successfully"
