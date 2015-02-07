4cd73205d6625f4534c863de83f0212d90571ed8
Sebastian Hüther <sebastian.huether@gmx.de>
2015-02-07 23:41:04 +0100

    TRANSPORT_LAYER: checksums inside the state machines; more states

--- TRANSPORT_LAYER/TRANSPORT_LAYER.vhd
+++ TRANSPORT_LAYER/TRANSPORT_LAYER.vhd
@@ -80,13 +80,17 @@ architecture rtl of TRANSPORT_LAYER is
         state                   : send_state_type;
         packet_out              : std_ulogic_vector(7 downto 0);
         packet_out_valid        : std_ulogic;
-        send_buf_wr_addr        : std_ulogic_vector(log2(BUFFERED_PACKETS)+2 downto 0);
-        send_buf_rd_addr        : std_ulogic_vector(log2(BUFFERED_PACKETS)+2 downto 0);
-        rst_checksum            : boolean;
+        send_buf_wr_addr        : std_ulogic_vector(log2(BUFFERED_PACKETS)+7 downto 0);
+        send_buf_rd_addr        : std_ulogic_vector(log2(BUFFERED_PACKETS)+7 downto 0);
+        packet_length           : unsigned(7 downto 0);
+        packet_index            : unsigned(log2(BUFFERED_PACKETS)-1 downto 0);
+        bytes_left_counter      : unsigned(8 downto 0);
         next_packet_number      : unsigned(7 downto 0);
         packet_records_p        : natural range 0 to BUFFERED_PACKETS;
         packet_records_wr_en    : boolean;
         timeout_ack             : std_ulogic_vector(BUFFERED_PACKETS-1 downto 0);
+        timeout_start           : std_ulogic_vector(BUFFERED_PACKETS-1 downto 0);
+        checksum                : std_ulogic_vector(7 downto 0);
     end record;
     
     constant send_reg_type_def   : send_reg_type := (
@@ -95,11 +99,15 @@ architecture rtl of TRANSPORT_LAYER is
         packet_out_valid        => '0',
         send_buf_wr_addr        => (others => '0'),
         send_buf_rd_addr        => (others => '0'),
-        rst_checksum            => true,
+        packet_length           => x"00",
+        packet_index            => (others => '0'),
+        bytes_left_counter      => (others => '0'),
         next_packet_number      => x"00",
         packet_records_p        => 0,
         packet_records_wr_en    => false,
-        timeout_ack             => (others => '0')
+        timeout_ack             => (others => '0'),
+        timeout_start           => (others => '0'),
+        checksum                => x"00"
     );
     
     signal cur_send_reg, next_send_reg  : send_reg_type := send_reg_type_def;
@@ -108,8 +116,6 @@ architecture rtl of TRANSPORT_LAYER is
     signal send_buf_valid   : std_ulogic := '0';
     signal send_buf_valid   : std_ulogic := '0';
     signal send_buf_count   : std_ulogic_vector(7 downto 0) := x"00";
-    
-    signal send_checksum    : std_ulogic_vector(7 downto 0) := x"00";
     
     --- receiving ---
     
@@ -122,11 +128,11 @@ architecture rtl of TRANSPORT_LAYER is
         recv_buf_wr_addr        : std_ulogic_vector(7 downto 0);
         recv_buf_rd_addr        : std_ulogic_vector(7 downto 0);
         recv_buf_rd_en          : std_ulogic;
-        rst_checksum            : boolean;
         packet_number           : unsigned(7 downto 0);
         next_packet_number      : unsigned(7 downto 0);
         packet_records_p        : natural range 0 to BUFFERED_PACKETS;
         packet_records_wr_en    : boolean;
+        checksum                : std_ulogic_vector(7 downto 0);
     end record;
     
     constant recv_reg_type_def  : recv_state_type := (
@@ -134,19 +140,17 @@ architecture rtl of TRANSPORT_LAYER is
         recv_buf_wr_addr        => x"00",
         recv_buf_rd_addr        => x"00",
         recv_buf_rd_en          => '0',
-        rst_checksum            => true,
         packet_number           => x"00",
         next_packet_number      => x"00",
         packet_records_p        => 0,
-        packet_records_wr_en    => false
+        packet_records_wr_en    => false,
+        checksum                => x"00"
     );
     
     signal cur_recv_reg, next_recv_reg  : recv_reg_type := recv_reg_type_def;
     
     signal recv_buf_dout    : std_ulogic_vector(7 downto 0) := x"00";
     signal recv_buf_valid   : std_ulogic := '0';
-    
-    signal recv_checksum    : std_ulogic_vector(7 downto 0) := x"00";
     
     --- packet records, accessed by both state machines ---
     
@@ -201,7 +205,7 @@ begin
     PACKET_OUT          <= cur_reg.packet_out;
     PACKET_OUT_VALID    <= cur_reg.packet_out_valid;
     
-    BUSY    <= '1' when cur_reg.state=WAITING_FOR_DATA else '0';
+    BUSY    <= '1' when cur_send_reg.state=WAITING_FOR_DATA else '0';
     
     send_buf_DUAL_PORT_RAM_inst : entity work.DUAL_PORT_RAM
         generic map (
@@ -237,28 +241,6 @@ begin
             DOUT    => recv_buf_dout
         );
     
-    send_checksum_proc : process(cur_send_reg.rst_checksum, CLK)
-    begin
-        if cur_send_reg.rst_checksum then
-            recv_checksum   <= x"00";
-        elsif rising_edge(CLK) then
-            if cur_reg.packet_out_valid='1' then
-                send_checksum   <= checksum+cur_reg.packet_out;
-            end if;
-        end if;
-    end process;
-    
-    recv_checksum_proc : process(cur_recv_reg.rst_checksum, CLK)
-    begin
-        if cur_recv_reg.rst_checksum then
-            recv_checksum   <= x"00";
-        elsif rising_edge(CLK) then
-            if cur_reg.packet_out_valid='1' then
-                recv_checksum   <= checksum+cur_reg.packet_out;
-            end if;
-        end if;
-    end process;
-    
     timeout_proc : process(RST, CLK)
         constant timeout_high   : natural := timeout_records_type.timeout'high;
     begin
@@ -282,11 +264,15 @@ begin
                     -- packet of which the acknowledge timed out was resent
                     pending_timeouts(i) <= '0';
                 end if;
+                
+                if cur_send_reg.timeout_start(i)='1' then
+                    timeout_records(i).is_active    <= true;
+                end if;
             end loop;
         end if;
     end process;
     
-    send_stm_proc : process(RST, cur_send_reg)
+    send_stm_proc : process(RST, cur_send_reg, pending_timeouts, SEND, DIN_WR_EN)
         alias cr is cur_send_reg;
         variable r  : send_reg_type := send_reg_type_def;
         
@@ -314,36 +300,56 @@ begin
         r   := cr;
         
         r.packet_out_valid  := '0';
-        r.rst_checksum      := false;
+        r.timeout_ack       := (others => '0');
+        r.timeout_start     := (others => '0');
+        
+        if DIN_WR_EN='1' then
+            r.packet_length := cr.packet_length+1;
+        end if;
         
         case cr.state is
             
             when WAITING_FOR_DATA =>
-                if SEND='1' and send_buf_empty='0' then
+                if pending_timeouts/=(pending_timeouts'range => '0') then
+                    r.state := EVALUATING_PACKET_ADDR_TO_RESEND;
+                end if;
+                if SEND='1' then
                     r.state := SENDING_DATA_PACKET_MAGIC;
                 end if;
             
             when SENDING_DATA_PACKET_MAGIC =>
+                r.checksum  := uns(DATA_MAGIC);
                 send_packet_byte(DATA_MAGIC, SENDING_DATA_PACKET_NUMBER);
             
             when SENDING_DATA_PACKET_NUMBER =>
+                r.checksum  := cr.checksum+cr.next_packet_number;
                 send_packet_byte(cr.next_packet_number, SENDING_DATA_PACKET_LENGTH);
             
             when SENDING_DATA_PACKET_LENGTH =>
-                r.send_buf_rd_en    := '1';
-                send_packet_byte(send_buf_count, SENDING_DATA_PACKET_PAYLOAD);
+                r.send_buf_rd_en        := '1';
+                r.bytes_left_counter    := cr.packet_length-1;
+                r.checksum              := cr.checksum+cr.packet_length;
+                send_packet_byte(stdulv(cr.packet_length), SENDING_DATA_PACKET_PAYLOAD);
             
             when SENDING_DATA_PACKET_PAYLOAD =>
-                r.send_buf_rd_en    := '1';
-                send_packet_byte(send_buf_dout, WAITING_FOR_ACK_MAGIC, send_buf_empty);
+                r.send_buf_rd_en        := '1';
+                r.bytes_left_counter    := cr.bytes_left_counter-1;
+                r.checksum              := cr.checksum+send_buf_dout;
+                send_packet_byte(send_buf_dout, SENDING_CHECKSUM, cr.bytes_left_counter(8)='1');
             
-            when WAITING_FOR_ACK_MAGIC =>
-                r.recv_buf_rd_en    := '1';
-                if recv_buf_valid='1' then
-                    if recv_buf_dout=ACK_MAGIC then
-                        r.state := WAITING_FOR_DATA;
+            when SENDING_CHECKSUM =>
+                r.timeout_start(int(cr.packet_index))   := '1';
+                send_packet_byte(stdulv(cr.checksum), WAITING_FOR_DATA);
+            
+            when EVALUATING_PACKET_ADDR_TO_RESEND =>
+                for i in 0 to BUFFERED_PACKETS-1 loop
+                    if pending_timeouts(i)='1' then
+                        r.packet_index      := uns(i, log2(BUFFERED_PACKETS));
+                        r.send_buf_rd_addr  := stdulv(i, log2(BUFFERED_PACKETS)) & x"00";
+                        r.state             := SENDING_DATA_PACKET_MAGIC;
+                        r.timeout_ack       := (i => '1', others => '0');
                     end if;
-                end if;
+                end loop;
             
         end case;
         
@@ -361,7 +367,6 @@ begin
         r   := cr;
         
         r.recv_buf_wr_en    := '0';
-        r.rst_checksum      := false;
         
         case cr.state is
             
@@ -374,24 +379,25 @@ begin
                     elsif PACKET_DIN=RESEND_MAGIC then
                         r.state := CHECKING_RESEND_PACKET_NUMBER;
                     end if;
+                    r.checksum  := uns(PACKET_DIN);
                 end if;
             
             when CHECKING_DATA_PACKET_NUMBER =>
-                
+                r.checksum  := cr.checksum+PACKET_DIN;
             
             when CHECKING_ACK_PACKET_NUMBER =>
                 r.packet_number := uns(PACKET_DIN);
+                r.checksum      := cr.checksum+PACKET_DIN;
                 r.state         := COMPARING_ACK_CHECKSUM;
             
             when CHECKING_RESEND_PACKET_NUMBER =>
-                
+                r.checksum  := cr.checksum+PACKET_DIN;
             
             when COMPARING_ACK_CHECKSUM =>
-                if checksum=PACKET_DIN then
+                if cr.checksum=PACKET_DIN then
                     r.ack_packets(int(PACKET_DIN))  := true;
                 end if;
-                r.rst_checksum  := true;
-                r.state         := WAITING_FOR_DATA;
+                r.state     := WAITING_FOR_DATA;
             
         end case;
         
