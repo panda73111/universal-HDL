12081c84735c5864e5939911daa7671d44fb8e9d
Sebastian Hüther <sebastian.huether@gmx.de>
2015-11-13 11:10:55 +0100

    Continued rewrite of test_spi_flash
    I was trying out caching the init files according to their address, but forgot that the flash has to be writable... I'll need a temporary file

--- test_spi_flash.vhd
+++ test_spi_flash.vhd
@@ -19,7 +19,6 @@ use work.txt_util.all;
 
 entity test_spi_flash is
     generic (
-        BYTE_COUNT          : positive := 1024;
         INIT_FILE_0_PATH    : string := "";
         INIT_FILE_0_ADDR    : std_ulogic_vector(23 downto 0) := x"000000";
         INIT_FILE_1_PATH    : string := "";
@@ -29,7 +28,8 @@ entity test_spi_flash is
         INIT_FILE_3_PATH    : string := "";
         INIT_FILE_3_ADDR    : std_ulogic_vector(23 downto 0) := x"000000";
         INIT_FILE_4_PATH    : string := "";
-        INIT_FILE_5_ADDR    : std_ulogic_vector(23 downto 0) := x"000000";
+        INIT_FILE_4_ADDR    : std_ulogic_vector(23 downto 0) := x"000000";
+        BUFFER_SIZE         : positive := 256;
         ERASE_TIME          : time := 2 ms; -- more realistic erase time: 800 ms. Ain't nobody got time for that...
         PROGRAM_TIME        : time := 800 us;
         VERBOSE             : boolean := false
@@ -46,47 +46,140 @@ architecture behavioral of test_spi_flash is
 
     constant INIT_FILE_COUNT    : positive := 5;
 
-    type flash_mem_type is
-        array(0 to BYTE_COUNT-1) of
-        std_ulogic_vector(7 downto 0);
-    signal flash_mem    : flash_mem_type  := (others => x"00");
+    type init_file_paths_type is
+        array(0 to INIT_FILE_COUNT) of
+        string;
+    constant INIT_FILE_PATHS    : init_file_paths_type := (
+        INIT_FILE_0_PATH,
+        INIT_FILE_1_PATH,
+        INIT_FILE_2_PATH,
+        INIT_FILE_3_PATH,
+        INIT_FILE_4_PATH
+    );
 
-    procedure initialize(signal flash_mem : out flash_mem_type) is
-        file f                  : TEXT;
-        variable l              : line;
-        variable char           : character;
-        variable val            : std_ulogic_vector(3 downto 0);
-        variable addr           : natural range 0 to BYTE_COUNT-1;
-        variable good           : boolean;
-        variable byte_complete  : boolean;
+    type init_file_addrs_type is
+        array(0 to INIT_FILE_COUNT) of
+        std_ulogic_vector(23 downto 0);
+    constant INIT_FILE_ADDRS    : init_file_addrs_type := (
+        INIT_FILE_0_ADDR,
+        INIT_FILE_1_ADDR,
+        INIT_FILE_2_ADDR,
+        INIT_FILE_3_ADDR,
+        INIT_FILE_4_ADDR
+    );
+
+    type buffer_type is
+        array(0 to BUFFER_SIZE-1) of
+        std_ulogic_vector(7 downto 0);
+    signal buf  : buffer_type := (others => x"00");
+
+    function addr_to_init_file_index(
+        addr : std_ulogic_vector(23 downto 0)
+    ) return integer is
+        variable highest_matching_address   : std_ulogic_vector(23 downto 0);
+        variable highest_matching_index     : integer;
     begin
-        if INIT_FILE_PATH'length/=0 then
-            assert not VERBOSE
-                report "Opening file: " & INIT_FILE_PATH
-                severity NOTE;
-            addr    := int(INIT_ADDR);
-            file_open(f, INIT_FILE_PATH, read_mode);
-            byte_complete   := false;
-            while not endfile(f) loop
-                readline(f, l);
-                read(l, char, good);
-                while good loop
-                    val := hex_to_stdulv(char);
-                    if byte_complete then
-                        flash_mem(addr)(3 downto 0) <= val;
-                        addr    := addr+1;
-                    else
-                        flash_mem(addr)(7 downto 4) <= val;
-                    end if;
-                    byte_complete   := not byte_complete;
-                    read(l, char, good);
-                end loop;
-            end loop;
-            assert not VERBOSE
-                report "Closing file: " & INIT_FILE_PATH
-                severity NOTE;
-            file_close(f);
+        highest_matching_index  := -1;
+
+        -- search for at least one init address below or at the given one
+        for i in 0 to INIT_FILE_COUNT loop
+            if addr >= INIT_FILE_ADDRS(i) then
+                highest_matching_index      := i;
+                highest_matching_address    := INIT_FILE_ADDRS(i);
+                exit;
+            end if;
+        end loop;
+
+        if highest_matching_index=-1 then
+            -- no init file for the given address
+            return -1;
         end if;
+
+        -- search the highest address below or at the given one
+        for i in 0 to INIT_FILE_COUNT loop
+            if
+                INIT_FILE_ADDRS(i) > highest_matching_address and
+                addr >= INIT_FILE_ADDRS(i)
+            then
+                highest_matching_index      := i;
+                highest_matching_address    := INIT_FILE_ADDRS(i);
+            end if;
+        end loop;
+
+        return highest_matching_index;
+    end function;
+
+    procedure fill_buffer(
+            signal buf          : out buffer_type;
+            variable start_addr : in std_ulogic_vector(23 downto 0)
+    ) is
+        variable init_file_index    : integer;
+        variable init_file_path     : string;
+        variable bytes_to_skip      : natural;
+        file f                      : TEXT;
+        variable l                  : line;
+        variable char               : character;
+        variable val                : std_ulogic_vector(3 downto 0);
+        variable buf_i              : natural range 0 to buffer_type'length-1;
+        variable good               : boolean;
+        variable byte_complete      : boolean;
+    begin
+        init_file_index  := addr_to_init_file_index(start_addr);
+        if init_file_index=-1 then
+            for i in 0 to buffer_type'length-1 loop
+                buf(i)  <= x"00";
+            end loop;
+            return;
+        end if;
+
+        init_file_path  := INIT_FILE_PATHS(init_file_index);
+
+        assert not VERBOSE
+            report "Opening file: " & init_file_path
+            severity NOTE;
+
+        bytes_to_skip   := int(start_addr-INIT_FILE_ADDRS(init_file_index));
+        buf_i           := 0;
+        byte_complete   := false;
+
+        file_open(f, init_file_path, read_mode);
+        file_loop : while not endfile(f) loop
+            readline(f, l);
+            read(l, char, good);
+            while good loop
+                val := hex_to_stdulv(char);
+                if byte_complete then
+                    buf(buf_i)(3 downto 0)  <= val;
+
+                    if bytes_to_skip=0 then
+                        buf_i   := buf_i+1;
+                    else
+                        bytes_to_skip   := bytes_to_skip-1;
+                    end if;
+                else
+                    buf(buf_i)(7 downto 4)  <= val;
+                end if;
+
+                exit file_loop when buf_i=buffer_type'length;
+
+                byte_complete   := not byte_complete;
+                read(l, char, good);
+            end loop;
+        end loop;
+
+        assert not VERBOSE
+            report "Closing file: " & init_file_path
+            severity NOTE;
+
+        file_close(f);
+
+        if buf_i!=buffer_type'length then
+            -- fill the rest with zeros
+            for i in buf_i to buffer_type'length-1 loop
+                buf(i)  <= x"00";
+            end loop;
+        end if;
+
     end procedure;
 
 begin
@@ -98,6 +191,8 @@ begin
         constant CMD_READ_DATA_BYTES        : cmd_type := x"03";
         constant CMD_PAGE_PROGRAM           : cmd_type := x"02";
         constant CMD_READ_STATUS_REGISTER   : cmd_type := x"05";
+
+        variable buffer_start_addr  : std_ulogic_vector(23 downto 0);
         variable flash_cmd          : cmd_type;
         variable flash_addr         : std_ulogic_vector(23 downto 0);
         variable flash_data_byte    : std_ulogic_vector(7 downto 0);
@@ -112,7 +207,7 @@ begin
             bit_loop : for i in 7 downto 1 loop
                 flash_cmd(i)    := MISO;
                 wait until rising_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
             end loop;
             flash_cmd(0)    := MISO;
         end procedure;
@@ -122,7 +217,7 @@ begin
             bit_loop : for i in 23 downto 1 loop
                 flash_addr(i)   := MISO;
                 wait until rising_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
             end loop;
             flash_addr(0)   := MISO;
             flash_addr      := stdulv(int(flash_addr) mod BYTE_COUNT, 24);
@@ -133,7 +228,7 @@ begin
             bit_loop : for i in 7 downto 1 loop
                 flash_data_byte(i)  := MISO;
                 wait until rising_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
             end loop;
             flash_data_byte(0)  := MISO;
         end procedure;
@@ -142,10 +237,10 @@ begin
         begin
             bit_loop : for i in 7 downto 1 loop
                 wait until falling_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
                 MOSI    <= flash_status(i);
                 wait until rising_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
             end loop;
             if SN='0' then
                 wait until falling_edge(C) or SN='1';
@@ -157,10 +252,10 @@ begin
         begin
             bit_loop : for i in 7 downto 1 loop
                 wait until falling_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
                 MOSI    <= flash_mem(int(flash_addr))(i);
                 wait until rising_edge(C) or SN='1';
-                if SN='1' then exit bit_loop; end if;
+                exit bit_loop when SN='1';
             end loop;
             if SN='0' then
                 wait until falling_edge(C) or SN='1';
@@ -168,7 +263,8 @@ begin
             end if;
         end procedure;
     begin
-        initialize(flash_mem);
+        buffer_start_addr   := x"000000";
+        fill_buffer(buf, buffer_start_addr);
 
         flash_status    := x"00";
         main_loop : loop
@@ -237,7 +333,7 @@ begin
                 end case;
 
                 wait until rising_edge(C) or SN='1';
-                if SN='1' then next main_loop; end if;
+                next main_loop when SN='1';
                 get_addr;
                 flash_addr  := flash_addr mod BYTE_COUNT;
                 assert not VERBOSE
@@ -285,7 +381,7 @@ begin
 
                     when CMD_PAGE_PROGRAM =>
                         wait until rising_edge(C) or SN='1';
-                        if SN='1' then next main_loop; end if;
+                        next main_loop when SN='1';
                         while SN='0' loop
                             get_data_byte;
                             if SN='1' then
